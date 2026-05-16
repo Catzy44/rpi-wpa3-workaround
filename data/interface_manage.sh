f74b86ffa3c8a6decff0df680e414e34e9f36605
catzy44 <przemowi2002@gmail.com>
2026-05-16 16:57:31 +0200

    feat: Installer is friendlier!

--- data/interface_manage.sh
+++ data/interface_manage.sh
@@ -2,5 +2,10 @@
 set -e
 cd "$(dirname "$0")"
 source ./var.sh
+S="[interface/manage]"
 
-rm -f "$NM_UNMANAGED_FILE"
+echo "$S[0] - unmanaging the interface..."
+
+sudo rm -f "$NM_UNMANAGED_FILE"
+
+echo "$S[1] - done!"
--- data/interface_unmanage.sh
+++ data/interface_unmanage.sh
@@ -2,10 +2,15 @@
 set -e
 cd "$(dirname "$0")"
 source ./var.sh
+S="[interface/unmanage]"
 
+echo "$S[0] - making sure NM/conf.d is created..."
 mkdir -p /etc/NetworkManager/conf.d
 
+echo "$S[1] - unmanaging the interface..."
 cat > "$NM_UNMANAGED_FILE" <<EOF
 [keyfile]
 unmanaged-devices=interface-name:${IFACE}
 EOF
+
+echo "$S[2] - done!"
